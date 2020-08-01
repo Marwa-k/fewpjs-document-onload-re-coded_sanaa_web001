92559c816ae792baea39b7a95cf132d341ef865c
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-01 21:31:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+console.log("This is really cool!");
 );
